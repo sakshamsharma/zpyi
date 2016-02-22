f6fab300d76429142bee192311ad27c1f24a3ea4
Saksham Sharma <saksham0808@gmail.com>
2016-02-23 01:41:11 +0530

    Added installation script and arranged code.

---
+++ install_script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd ~
+git clone https://github.com/sakshamsharma/zpyi ~/.zpyi
+echo "source ~/.zpyi/zpyi.zsh" >> ~/.zshrc
--- zpyi.zsh
+++ zpyi.zsh
@@ -6,13 +6,13 @@
 ###########################################
 
 command_not_found_handler() {
-  ei=$(eix -Q "$1" | grep '^No\ matches\ found\.$')
+  ei=$(~/.zpyi/package_search_command.zsh "$1" | grep '^No\ matches\ found\.$')
   if [ -z "$ei" ]
   then
-    echo "Command not found. Searching for package using eix..."
-    eix -Q "$1"
+    echo "Command not found. Searching for package using your distro's package list..."
+    ~/.zpyi/package_search_command.zsh "$1"
   else
-    echo "$1" | python $MYZSH/zsh_py.py
+    echo "$1" | python ~/.zpyi/zpyi.py
     if [ $? -ne 0 ]
     then
       echo "zsh, python: Command not found => $1"
